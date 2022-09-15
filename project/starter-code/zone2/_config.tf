206b2296fdb26287b660e2e7f91e18849a30d5d8
beginnerbikari-369 <chandramoulichukka2000@gmail.com>
2022-09-16 02:23:13 +0530

    new one 7

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-chandu-west"
+     bucket = "udacity-tf-chandu-west1"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
