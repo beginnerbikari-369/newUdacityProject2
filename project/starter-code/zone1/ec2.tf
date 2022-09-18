1cfafdde49beffe7d573f6a80e406a1c91d4856f
beginnerbikari-369 <chandramoulichukka2000@gmail.com>
2022-09-18 16:21:33 +0530

    c

--- project/starter-code/zone1/ec2.tf
+++ project/starter-code/zone1/ec2.tf
@@ -3,7 +3,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0c19ccdd4a80213ed"
+   aws_ami            = "ami-05218ecd0f7188bf7"
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
--- project/starter-code/zone2/ec2.tf
+++ project/starter-code/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0a7d7c1c057bf16d1"
+   aws_ami            = "ami-0f002caba5fec565d"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
    public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
